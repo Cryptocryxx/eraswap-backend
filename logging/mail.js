4899efa27ee1edfdde3f8352dc42131fb4a6d8a2
L Bauscher <143177997+Cryptocryxx@users.noreply.github.com>
2025-12-16 15:12:29 +0000

    Update mail.js

--- logging/mail.js
+++ logging/mail.js
@@ -1,27 +1,23 @@
 import nodemailer from 'nodemailer';
 
-function sendEmail(subject, text, toEmail, fromEmail = "Eraswap Support") {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'lori.bauscher@gmail.com',
-          pass: "gyyb ewkg briz nkpd"
-      }
-    });
-    // E-Mail senden
-    transporter.sendMail({
-      from: `"${fromEmail}" <lori.bauscher@gmail.com>`, // Absender
-      to: toEmail, // Empfänger
-      subject: subject,
-      text: text, // Nachricht im Textformat
-      html: `<h1>${text}</h1>` // Nachricht im HTML-Format
-    }, (error, info) => {
-      if (error) {
-        throw error
-      }
-    });
-    return "Nachricht erfolgreich gesendet!"
+async function sendEmail(subject, html, toEmail, fromEmail = "Eraswap Support") {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'lori.bauscher@gmail.com',
+      pass: 'gyyb ewkg briz nkpd'
+    }
+  });
+
+  return transporter.sendMail({
+    from: `"${fromEmail}" <lori.bauscher@gmail.com>`,
+    to: toEmail,
+    subject: subject,
+    text: 'Please verify your EraSwap account.',
+    html: html
+  });
 }
+
 
 export default sendEmail;
 
