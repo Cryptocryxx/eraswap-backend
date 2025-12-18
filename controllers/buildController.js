33eeb2591c2900a1f04ceb53df069ad09e93d8cd
L Bauscher <143177997+Cryptocryxx@users.noreply.github.com>
2025-12-18 20:19:40 +0000

    Update buildController.js

--- controllers/buildController.js
+++ controllers/buildController.js
@@ -21,13 +21,13 @@ async function build(platform, userId, res) {
         }
 
         if (res && filePath) {
+             logger.info(`Build for platform: ${platform}, user ID: ${userId} completed successfully.`);
+        logger.info(`Notification email sent.`);
+        await sendEmail('Build Downloaded', `<h1>${user.username} with ${user.email} has downloaded the ${platform} build.</h1>`, "l.bauscher@icloud.com", "l.bauscher@gmail.com");
             return res.download(filePath);
         }
 
-        logger.info(`Build for platform: ${platform}, user ID: ${userId} completed successfully.`);
-        logger.info(`Notification email sent.`);
-        await sendEmail('Build Downloaded', `<h1>${user.username} with ${user.email} has downloaded the ${platform} build.</h1>`, "l.bauscher@icloud.com", "l.bauscher@gmail.com");
-    } catch (error) {
+       } catch (error) {
         logger.error(`Build error for platform: ${platform}, user ID: ${userId}: ${error.message}`);
         if (res) return res.status(500).send('Build failed');
     }
